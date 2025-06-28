31a107375c1a17c32d6cde407629e647b92b820b
bharathi-025 <bharathisivakumar25@gmail.com>
2025-06-28 08:16:48 +0530

    stripe added check

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,85 +1,84 @@
-// import Stripe from "stripe"
-// import Course from "../models/course.js"
-// import { Purchase } from "../models/purchase.js"
-// import User from "../models/user.js"
+import Stripe from "stripe"
+import Course from "../models/course.js"
+import { Purchase } from "../models/purchase.js"
+import User from "../models/user.js"
 
-// //Get user data
-// export const getUserData=async(req,res)=>{
-//     try {
-//         const userId=req.auth.userId
-//         const user=await User.findById(userId)
+//Get user data
+export const getUserData=async(req,res)=>{
+    try {
+        const userId=req.auth.userId
+        const user=await User.findById(userId)
 
-//         if(!user){
-//             return res.json({success:false,message:'User not found'})
-//         }
-//         res.json({success:true, user})
-//     } catch (error) {
-//         res.json({success:false, message:error.message})
-//     }
-// }
+        if(!user){
+            return res.json({success:false,message:'User not found'})
+        }
+        res.json({success:true, user})
+    } catch (error) {
+        res.json({success:false, message:error.message})
+    }
+}
 
-// //User enrolled courses with lecture links
-// export const userEnrolledCourses= async(req,res)=>{
-//     try{
-//         const userId=req.auth.userId
-//         const userData=await User.findById(userId).populate('enrolledCourses')
-//         res.json({success:true, enrolledCourses:userData.enrolledCourses})
-//     }catch(error){
-//         res.json({success:false,message:error.message})
-//     }
-// }
+//User enrolled courses with lecture links
+export const userEnrolledCourses= async(req,res)=>{
+    try{
+        const userId=req.auth.userId
+        const userData=await User.findById(userId).populate('enrolledCourses')
+        res.json({success:true, enrolledCourses:userData.enrolledCourses})
+    }catch(error){
+        res.json({success:false,message:error.message})
+    }
+}
 
-// //Purchase course
-// export const purchaseCourse=async(req,res)=>{
-//     try {
-//         const {courseId} =req.body
-//         const {origin} =req.headers
-//         const userId=req.auth.userId
-//         const courseData= await Course.findById(courseId)
-//         const userData=await User.findById(userId)
+//Purchase course
+export const purchaseCourse=async(req,res)=>{
+    try {
+        const {courseId} =req.body
+        const {origin} =req.headers
+        const userId=req.auth.userId
+        const courseData= await Course.findById(courseId)
+        const userData=await User.findById(userId)
 
-//         if(!userData||!courseData){
-//             return res.json({success:false,message:error.message})
-//         }
+        if(!userData||!courseData){
+            return res.json({success:false,message:error.message})
+        }
 
-//         const purchaseData={
-//             courseId : courseData._id,
-//             userId,
-//             amount:(courseData.coursePrice-courseData.discount*courseData.coursePrice/100).toFixed(2)
-//         }
+        const purchaseData={
+            courseId : courseData._id,
+            userId,
+            amount:(courseData.coursePrice-courseData.discount*courseData.coursePrice/100).toFixed(2)
+        }
 
-//         const newPurchase= await Purchase.create(purchaseData)
+        const newPurchase= await Purchase.create(purchaseData)
 
-//         //Stripe gateway initialize
-//         const stripInstance=new Stripe(process.env.STRIPE_SECRET_KEY)
-//         const currency=process.env.CURRENCY.toLowerCase()
+        //Stripe gateway initialize
+        const stripInstance=new Stripe(process.env.STRIPE_SECRET_KEY)
+        const currency=process.env.CURRENCY.toLowerCase()
 
-//         //creating line items to for stripe
-//         const line_items=[{
-//             price_data:{
-//                 currency,
-//                 product_data:{
-//                     name:courseData.courseTitle
-//                 },
-//                 unit_amount:Math.floor(newPurchase.amount)*100
-//             },
-//             quantity:1
-//         }]
+        //creating line items to for stripe
+        const line_items=[{
+            price_data:{
+                currency,
+                product_data:{
+                    name:courseData.courseTitle
+                },
+                unit_amount:Math.floor(newPurchase.amount)*100
+            },
+            quantity:1
+        }]
 
-//         const session=await stripInstance.checkout.sessions.create({
-//             success_url:`${origin}/loading/my-enrollments`,
-//             cancel_url:`${origin}/`,
-//             mode:'payment',
-//             line_items,
-//             payment_intent_data:{
-//             metadata:{
-//                 purchaseId: newPurchase._id.toString()
-//             }
-//         }
-//         })
-//         res.json({success:true,session_url:session.url})
+        const session=await stripInstance.checkout.sessions.create({
+            success_url:`${origin}/loading/my-enrollments`,
+            cancel_url:`${origin}/`,
+            line_items:line_items,
+            mode:'payment',
+            metadata:{
+                purchaseId: newPurchase._id.toString()
+            }
+        
+        })
+        res.json({success:true,session_url:session.url})
 
-//     } catch (error) {
-//         res.json({success:false, message:error.message})
-//     }
-// }
+    } catch (error) {
+        res.json({success:false, message:error.message})
+    }
+}
--- server/server.js
+++ server/server.js
@@ -3,30 +3,30 @@ import cors from 'cors'
 import 'dotenv/config'
 import connetDB from './configs/mongodb.js'
 import { clerkWebHooks } from './controllers/webhooks.js'
-// import { clerkMiddleware } from '@clerk/express'
-// import educatorRouter from './routes/educatorRoutes.js'
-// import connectCloudinary from './configs/cloudinary.js'
-// import courseRouter from './routes/courseRoutes.js'
-// import userRouter from './routes/userRoutes.js'
+import { clerkMiddleware } from '@clerk/express'
+import educatorRouter from './routes/educatorRoutes.js'
+import connectCloudinary from './configs/cloudinary.js'
+import courseRouter from './routes/courseRoutes.js'
+import userRouter from './routes/userRoutes.js'
 
 //Intialize Express
 const app=express()
 
 //connect to database
 await connetDB()
-// await connectCloudinary()
+await connectCloudinary()
 
 //Middleware
 app.use(cors())
-// app.use(clerkMiddleware())
+app.use(clerkMiddleware())
 
 //Routes
 app.get('/',(req,res)=>res.send("API working"))
 app.post('/clerk', express.json(), clerkWebHooks)
-// app.use('/api/educator',express.json(),educatorRouter)
-// app.use('/api/course',express.json(),courseRouter)
-// app.use('/api/user',express.json(),userRouter)
-//app.post('/stripe',express.raw({type:'application/json'}),stripeWebhooks)
+app.use('/api/educator',express.json(),educatorRouter)
+app.use('/api/course',express.json(),courseRouter)
+app.use('/api/user',express.json(),userRouter)
+app.post('/stripe',express.raw({type:'application/json'}),stripeWebhooks)
 
 //Port
 const PORT=process.env.PORT || 5000
